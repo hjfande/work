d49156f35002eb2691bf96f4c9323c04b8fed2cf
User <user@example.com>
2026-06-18 15:45:41 +0800

    不再将tr.address[31:16]设置为0

--- efuse_ctrl/efuse_ctrl_scoreboard.sv
+++ efuse_ctrl/efuse_ctrl_scoreboard.sv
@@ -1358,7 +1358,6 @@ task efuse_ctrl_scoreboard::apbs_checker();
 
   forever begin
     apbs_port.get(tr);
-    tr.address[31:16] = 16'h0;
 
     `uvm_info(get_type_name(), $sformatf(
       "[APBS] %s addr=0x%08x data=0x%08x",
@@ -1535,7 +1534,6 @@ task efuse_ctrl_scoreboard::apbp_checker();
 
   forever begin
     apbp_port.get(tr);
-    tr.address[31:16] = 16'h0;
 
     `uvm_info(get_type_name(), $sformatf(
       "[APBP] %s addr=0x%08x data=0x%08x",
